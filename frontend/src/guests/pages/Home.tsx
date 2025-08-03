e5179c7f4210d14290b1a7e631b2c2be0d2f530c
Kent Vincent Malaran <kentvincentmalaran46@gmail.com>
2025-08-03 20:04:52 +0800

    feat: add robux logo and change title

--- frontend/src/guests/pages/Home.tsx
+++ frontend/src/guests/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { ArrowTrendingUpIcon, BanknotesIcon } from "@heroicons/react/24/solid";
+import { ArrowTrendingUpIcon } from "@heroicons/react/24/solid";
 
 import GuestLayout from "../../layouts/GuesLayout";
 import { cardData } from "../variables/data";
@@ -11,11 +11,12 @@ export default function Home() {
             <div className="flex items-center justify-center px-4" style={{ minHeight: "calc(100vh - 64px)" }}>
                 <div className="bg-white/5 backdrop-blur-md border border-white/10 rounded-xl p-8 w-full max-w-md text-center shadow-lg">
                     <div className="flex justify-center mb-4">
-                        <BanknotesIcon className="h-12 w-12 text-yellow-400" />
+                        {/* <BanknotesIcon className="h-12 w-12 text-yellow-400" /> */}
+                        <img className="w-30 h-30" src="/assets/img/robux_logo.png" />
                     </div>
-                    <h1 className="text-2xl font-semibold text-white mb-2">Total Supplied Value</h1>
+                    <h1 className="text-2xl font-semibold text-white mb-2">Total Robux Acquired</h1>
                     <p className="text-3xl font-bold text-yellow-500">
-                        ₱{totalSuppliedValue.toLocaleString()}
+                        {totalSuppliedValue.toLocaleString()}
                     </p>
                     <div className="flex items-center justify-center gap-2 mt-4 text-sm text-gray-300">
                         <ArrowTrendingUpIcon className="h-5 w-5 text-green-400" />
